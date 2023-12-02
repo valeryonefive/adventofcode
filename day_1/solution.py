024c18f817557b8ad4ad4a17172c089619bc086f
valerybriz <valery@one-five.com>
2023-12-02 16:34:33 +0100

    fixes and part two finished

--- day_1/solution.py
+++ day_1/solution.py
@@ -1,7 +1,11 @@
+import regex as re
 from utilities import input_to_list, clean_text
 
 
-def search_number(text, is_reverse=False):
+NUMBERS = {"one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+
+def search_number_first_approach(text: str, is_reverse=False) -> str:
     if is_reverse:
         text = text[::-1]
 
@@ -11,13 +15,31 @@ def search_number(text, is_reverse=False):
     raise Exception("Couldn't find any number")
 
 
+def normalize_number(current_number: str, reverse=False) -> str:
+    if current_number.isdigit():
+        if reverse:
+            return current_number[-1]
+        return current_number[0]
+    else:
+        return NUMBERS[current_number]
+
+
+def search_number_improved(text: str) -> int:
+    pattern = r"(^a(?=\s)|one|two|three|four|five|six|seven|eight|nine|[0-9]+)"
+    x = re.findall(pattern, text, overlapped=True)
+    if len(x) < 1:
+        raise Exception("No numbers found")
+    first_number = normalize_number(x[0])
+    last_number = normalize_number(x[-1], reverse=True)
+    return int(first_number + last_number)
+
+
 complete_list = []
 list_input = input_to_list("input.txt")
 clean_list = [clean_text(i) for i in list_input]
-for i, line in enumerate(clean_list):
-    first_number = search_number(clean_list[i])
-    second_number = search_number(clean_list[i], is_reverse=True)
-    complete_list.append(int(first_number + second_number))
+for line in clean_list:
+
+    complete_list.append(search_number_improved(line))
 
 print(sum(complete_list))
 
--- utilities.py
+++ utilities.py
@@ -1,7 +1,7 @@
-def input_to_list(path):
+def input_to_list(path: str) -> list:
     with open(path, "r") as f:
         return f.readlines()
 
 
-def clean_text(text):
+def clean_text(text: str) -> str:
     return text.strip()
